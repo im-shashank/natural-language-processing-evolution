fd03b1d9af25c82c8df97165c2042737006b702e
im-shashank <shashank.bhardwaj2003@gmail.com>
2026-07-01 23:41:28 +0530

    Added support for cuda

--- main/bigram_language_model.py
+++ main/bigram_language_model.py
@@ -4,7 +4,10 @@ from bigram_neuron import *
 from training_set import *
 
 # Check if MPS (Metal Performance Shaders) is available for Apple Silicon
-device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+device = torch.device("cuda" if torch.cuda.is_available()
+                      else "mps" if torch.backends.mps.is_available()
+                         else "cpu")
+print(f"Training on device: {device}")
 
 class BigramLanguageModel:
     """
